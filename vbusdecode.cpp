c01fe9d3d17ac8cc1cd5f944e493712c361c050e
andywhite <andywhite@c11c73ec-34a3-11de-b438-597f59cd7555>
2009-05-20 13:51:52 +0000

    Using scanf instead of fgets as at least once the controller gave a code that fgets thought was a newline code This raw.log was a capture with a newline

--- vbusdecode.cpp
+++ vbusdecode.cpp
@@ -1,13 +1,12 @@
 // sample  cat ../raw.log | ./a.out  0,15,0.1 2,15,0.1 4,15,0.1 6,15,0.1 8,7,1 9,7,1 20,16,1 22,16,1000 24,16,1000000
 // will give temp of s1,s2,s3,s4 pumpspeed pump1,pump2 and watts, Kw, Mw for a resol deltasol bs plus
-#include <string>
 #include <stdio.h>
 #include <iostream>
 using namespace std;
 
 int model;
 unsigned char frame[4];
-unsigned char allframes[256];
+unsigned char allframes[1026];
 
 void giveresults(char parray[])
 {
@@ -28,7 +27,9 @@ int decodeheader()
 	char  buffer[10];
 	unsigned char a;
 	unsigned char b;
-	if ( fgets(buffer, 10, stdin) != NULL)
+//	if ( fgets(buffer, 10, stdin) != NULL)
+	if (scanf("%c%c%c%c%c%c%c%c%c",&buffer[0],&buffer[1],&buffer[2],&buffer[3],&buffer[4],&buffer[5],&buffer[6],&buffer[7],&buffer[8],&buffer[9]) != 1)
+
 	{
 		a = buffer[0] + buffer[1] + buffer[2] + buffer[3] + buffer[4] + buffer[5] + buffer[6] + buffer[7];
 		b = ~ a;
@@ -48,20 +49,23 @@ int decodeheader()
 
 unsigned char decodeframe(int x)
 {
-	char  buffer[7];
+	char  buffer[7] ;
 	unsigned char a;
 	unsigned char b;
-	if ( fgets(buffer, 7, stdin) != NULL)
+//	if ( fgets(buffer, 7, stdin) != NULL)
+	if (scanf("%c%c%c%c%c%c",&buffer[0],&buffer[1],&buffer[2],&buffer[3],&buffer[4],&buffer[5]) != 1)
 	{
+
 		a = buffer[0]  + buffer[1]  + buffer[2] + buffer[3] + buffer[4] ;
+		b = 0;
 		b =  (( ~a | 0x80) - 0x80 ) ;
 		a = buffer[5];
 		if (  a == b )
 		{
+			frame[0] =  buffer[0] + (( buffer[4] & 0x01 ) * 0x80 ) ;
 			frame[1] =  buffer[1] + (( buffer[4] >> 1 & 0x01 ) * 0x80 ) ;
-			frame[0] =  buffer[0] + (( buffer[4] & 0x01 ) * 0x80 ) ; 
-			frame[3] =  buffer[3] + (( buffer[4] >> 3 & 0x01 ) * 0x80 ) ; 
 			frame[2] =  buffer[2] + (( buffer[4] >> 2 & 0x01 ) * 0x80 )  ;
+			frame[3] =  buffer[3] + (( buffer[4] >> 3 & 0x01 ) * 0x80 ) ; 
 		}
 		else
 		{
@@ -70,7 +74,6 @@ unsigned char decodeframe(int x)
 			frame[2] = 0;
 			frame[3] = 0;
 		}
-
 
 	}
 
@@ -83,6 +86,7 @@ int main(int argc, char* argv[])
 	char* arg_dup;
 	int framecount;
 	while ( fgets(buffer, 2, stdin) != NULL)
+//	while (scanf("%c",&buffer[0]) != 1)
 	{
 		a = buffer[0];
 		if ( a ==  0xAA) {
@@ -97,7 +101,6 @@ int main(int argc, char* argv[])
 					allframes[(4*x)+2] =frame[2];
 					allframes[(4*x)+3] =frame[3];
 				}
-
 				for(int i = 1; i < argc; i++)
 				{
 					arg_dup = strdup(argv[i]);
