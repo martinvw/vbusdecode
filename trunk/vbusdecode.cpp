662991d1cfb8455f3ea64e11b70142f0d1e1de31
andywhite@gmail.com <andywhite@gmail.com@c11c73ec-34a3-11de-b438-597f59cd7555>
2014-02-04 23:54:23 +0000

    Add option to debug add option to filter on source address (aka model id) Add option to output Fahrenheit, bit outputs as true/false/on/off/yes/no

--- trunk/vbusdecode.cpp
+++ trunk/vbusdecode.cpp
@@ -1,35 +1,46 @@
-// arguments [-c] [-f filename] [-c count]  fields
+// arguments [-d] [-f filename] [-c count]  fields
 // -f filename
-//	will put last retrieved value to filename , overwriting anyother contents
+//      will put last retrieved value to filename , overwriting anyother contents
 //
 // -c The number of full frames to decode, then exit
 //
+// -d Debug, prints more info
+//
+// -s filter for this source addr/model addr, value in hex 
+//
 // fields
-//	4 values , seperated by commas, where value
-// 	 	1 = offset from start of data, this value is bytes
-//		2 = length, how many bits
-//		3 =  if length > 1 this is the multiplier
-//			 if length = 1 this is the bit position within the byte
-//					use for bit fields, like error mask, relay mask etc.
-//		4 = format, p = plus , add the returned value to the next field
-//					t = time , format the output as time
-//					any other value is ignored.
-//					future will include c=*C, f=*F, l=logical (bit fields true/false), y = yes/no (bit fields)
+//      4 values , seperated by commas, where value
+//              1 = offset from start of data, this value is bytes
+//              2 = length, how many bits
+//              3 = bitposition or multiplier
+//                       if length > 1 this is the multiplier
+//                       if length = 1 this is the bit position within the byte
+//                                      use for bit fields, like error mask, relay mask etc.
+//              4 = format, p = plus , add the returned value to the next field
+//                          t = time , format the output as time
+//                          f = convert to Fahrenheit
+//                          l = convert bit fields to True/False
+//                          y = convert bit fields to Yes/No
+//                          o = convert bit fields to On/Off
+//                                      any other value is ignored.
 // to get this information for your model, check the wiki or look in the Vbus XMl file in Resol lite.
 //
 // example:
 // cat raw.log | ./a.out -f rrdvals -c 1 0,15,0.1 2,15,0.1 4,15,0.1 6,15,0.1 8,7,1 9,7,1 20,16,1,p 22,16,1000,p 24,16,1000000 12,16,0,t 10,1,0 10,1,1
 // will give temp of s1,s2,s3,s4 pumpspeed pump1,pump2 and total of watts, formatted system time, r1 and r2 status for a resol deltasol bs plus, put them into a file called rrdvals, and exit after decoding one frame successfully
-//45.7 24.6 49.8 29.1 100 0 2609964 14:36 1 0
+// rrdvals should contain
+// 45.7 24.6 49.8 29.1 100 0 2609964 14:36 1 0
+//            or
+// cat raw.log | ./a.out -s 0x4221 -c 1 0,15,0.1,f 2,15,0.1 4,15,0.1 6,15,0.1 8,7,1 9,7,1 20,16,1,p 22,16,1000,p 24,16,1000000 12,16,0,t 10,1,0,y 10,1,1,l
+// will output
+// 114.3 24.6 49.8 29.1 100 0 2609964 14:36 Yes False
+
 
 #include <stdio.h>
 #include <iostream>
-#include <stdlib.h>
-#include <cstring>
-
 using namespace std;
 
-int adding = 0,model=0;
+int debug = 0,adding = 0,model=0,modelid=0;
 float totals;
 string presults;
 unsigned char frame[4],allframes[256];
@@ -38,14 +49,23 @@ void giveresults(char parray[])
 {
     string formatter;
     char *format = NULL;
-    float f;
+    float f, Fahrenheit;
     char results[24];
     int offset = atoi(strtok (parray,","));
     int length = atoi(strtok (NULL, ","));
+
     if (length == 1) {
         int bitposition = atoi(strtok (NULL, ","));
         f = (((allframes[offset] >> bitposition) & 0x01 ) * 0x01);
-        sprintf(results, "%.0f ",f);
+        format = strtok (NULL, ",");
+        if (format != NULL) {
+            formatter = format;
+                if ( formatter == "l" ) {
+                    sprintf(results, "%s ",f ? "True" : "False");
+                } else if ( formatter == "y" ) {
+                    sprintf(results, "%s ",f ? "Yes" : "No");
+                } else if ( formatter == "o" ) sprintf(results, "%s ",f ? "On" : "Off");
+        } else sprintf(results, "%.0f ",f);
         presults.append(results);
     } else {
         float multiplier = atof(strtok (NULL, ","));
@@ -69,6 +89,11 @@ void giveresults(char parray[])
                 int hours = time/60;
                 int mins = time - (hours*60);
                 sprintf(results, "%02d:%02d ",hours,mins);
+            } else if (formatter == "f" ) {
+                Fahrenheit = (f * 1.8 ) + 32;
+                if (multiplier < 1) {
+                   sprintf(results, "%.1f ",Fahrenheit);
+                } else sprintf(results, "%.0f ",Fahrenheit);
             }
         } else if (multiplier < 1) {
             sprintf(results, "%.1f ",f);
@@ -85,21 +110,37 @@ void giveresults(char parray[])
 
 int decodeheader()
 {
-    char  buffer[10];
+    char  buffer[15];
     unsigned char a;
     unsigned char b;
     if (scanf("%c%c%c%c%c%c%c%c%c",&buffer[0],&buffer[1],&buffer[2],&buffer[3],&buffer[4],&buffer[5],&buffer[6],&buffer[7],&buffer[8]) != 1)
     {
         a = buffer[0] + buffer[1] + buffer[2] + buffer[3] + buffer[4] + buffer[5] + buffer[6] + buffer[7];
-        b = ~ a;
-        a = buffer[8];
+        if ( buffer[4] == 0x10 )
+        {
+          if ( debug == 1 ) std::cout << "Protocol Version 1.0\n";
+          b = ~ a;
+          a = buffer[8];
+        } else if ( buffer[4] == 0x20 ) {
+          if ( debug == 1 ) std::cout << "Protocol Version 2.0\n";
+          if ( buffer[6] * 0x100  + buffer[5] == 0x0500 )
+          {
+            if ( debug == 1 ) std::cout << "Broadcast for Clearance\n";
+          }
+          b = a;
+        } else {
+          if ( debug == 1 ) std::cout << "Unknown Protocol Version\n";
+        }
     }
+
     if (  a == b )
     {
         model = buffer[3] * 0x100  + buffer[2];
+        if ( debug == 1 ) fprintf(stdout,"Model: 0x%02x \n", model);
         a = buffer[7];
         return a;
     } else {
+        if ( debug == 1 ) std::cout << "Bad Header CRC\n";
         return 0;
     }
 }
@@ -126,6 +167,7 @@ int decodeframe(int x)
             frame[1] = 0;
             frame[2] = 0;
             frame[3] = 0;
+            if ( debug == 1 ) std::cout << "Bad Frame CRC\n";
         }
     }
     return a - b;
@@ -142,6 +184,7 @@ int main(int argc, char* argv[])
     {
         a = buffer[0];
         if ( a ==  0xAA) {
+            if ( debug == 1 ) std::cout << "SyncByte\n";
             framecount  =  decodeheader();
             if (framecount !=0 )
             {
@@ -166,15 +209,22 @@ int main(int argc, char* argv[])
                         } else if (sw == "-c") {
                             i++;
                             decodecount = atoi(argv[i]);
+                        } else if (sw == "-d") {
+                            debug=1;
+                        } else if (sw == "-s") {
+                            i++;
+                            modelid = strtol(argv[i],NULL,16);
                         } else {
+                            if (modelid == 0 || modelid == model) {
                             arg_dup = strdup(argv[i]);
                             giveresults(arg_dup);
                             free(arg_dup);
+                            }
                         }
                     }
                     if (filen == "stdout")
                     {
-                        fprintf(stdout,"%s\n",presults.c_str());
+                        if ( presults != "" ) fprintf(stdout,"%s\n",presults.c_str());
                     } else {
                         pFile = fopen (filen.c_str(),"w");
                         fprintf (pFile,"%s\n",presults.c_str());
@@ -182,6 +232,8 @@ int main(int argc, char* argv[])
                     }
                 }
                 presults = "";
+            } else {
+                if ( debug == 1 ) std::cout << "No Frames\n";
             }
         }
     }
