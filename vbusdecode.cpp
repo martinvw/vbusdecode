5ef3841d3d0fbb96a54893fa057a747da2cba51e
Martin van Wingerden <martin.van.wingerden@chess-ix.com>
2016-06-14 13:46:47 +0200

    Beautified the code

--- vbusdecode.cpp
+++ vbusdecode.cpp
@@ -36,85 +36,83 @@
 // 114.3 24.6 49.8 29.1 100 0 2609964 14:36 Yes False
 
 
-#include <stdio.h>
-#include <iostream>
-#include <stdlib.h>
-#include <cstring>
+#
+include < stdio.h > #include < iostream > #include < stdlib.h > #include < cstring >
 
-using namespace std;
+    using namespace std;
 
-int debug = 0,adding = 0,model=0,modelid=0;
+int debug = 0, adding = 0, model = 0, modelid = 0;
 float totals;
 string presults;
-unsigned char frame[4],allframes[256];
+unsigned char frame[4], allframes[256];
 
 void giveresults(char parray[]) {
     string formatter;
-    char *format = NULL;
+    char * format = NULL;
     float f;
     float Fahrenheit;
     char results[24];
-    int offset = atoi(strtok (parray,","));
-    int length = atoi(strtok (NULL, ","));
+    int offset = atoi(strtok(parray, ","));
+    int length = atoi(strtok(NULL, ","));
 
     if (length == 1) {
-        int bitposition = atoi(strtok (NULL, ","));
-        f = (((allframes[offset] >> bitposition) & 0x01 ) * 0x01);
-        format = strtok (NULL, ",");
+        int bitposition = atoi(strtok(NULL, ","));
+        f = (((allframes[offset] >> bitposition) & 0x01) * 0x01);
+        format = strtok(NULL, ",");
         if (format != NULL) {
             formatter = format;
-            if ( formatter == "l" ) {
-                sprintf(results, "%s ",f ? "True" : "False");
-            } else if ( formatter == "y" ) {
-                sprintf(results, "%s ",f ? "Yes" : "No");
-            } else if ( formatter == "o" ) sprintf(results, "%s ",f ? "On" : "Off");
+            if (formatter == "l") {
+                sprintf(results, "%s ", f ? "True" : "False");
+            } else if (formatter == "y") {
+                sprintf(results, "%s ", f ? "Yes" : "No");
+            } else if (formatter == "o") sprintf(results, "%s ", f ? "On" : "Off");
         } else {
-            sprintf(results, "%.0f ",f);
+            sprintf(results, "%.0f ", f);
         }
         presults.append(results);
     } else {
-        float multiplier = atof(strtok (NULL, ","));
-        if (((length -1) /24) ) {
-            int i = (allframes[offset] + (allframes[offset+1]*0x100) + (allframes[offset+2]*0x1000) + (allframes[offset+3]*0x10000) );
+        float multiplier = atof(strtok(NULL, ","));
+        if (((length - 1) / 24)) {
+            int i = (allframes[offset] + (allframes[offset + 1] * 0x100) + (allframes[offset + 2] * 0x1000) + (allframes[offset + 3] * 0x10000));
             f = i * multiplier;
-        } else if (((length -1) /16) ) {
-            int i = (allframes[offset] + (allframes[offset+1]*0x100) + (allframes[offset+2]*0x1000) );
+        } else if (((length - 1) / 16)) {
+            int i = (allframes[offset] + (allframes[offset + 1] * 0x100) + (allframes[offset + 2] * 0x1000));
             cout << i << "\n";
-            f = (allframes[offset] + (allframes[offset+1]*0x100) + (allframes[offset+2]*0x1000) ) * multiplier;
-        } else if (((length -1) /8) ) {
-            short i = (allframes[offset] + (allframes[offset+1]*0x100) );
+            f = (allframes[offset] + (allframes[offset + 1] * 0x100) + (allframes[offset + 2] * 0x1000)) * multiplier;
+        } else if (((length - 1) / 8)) {
+            short i = (allframes[offset] + (allframes[offset + 1] * 0x100));
             f = i * multiplier;
-        } else if (multiplier !=0 ) {
-            f = (allframes[offset] ) * multiplier;
+        } else if (multiplier != 0) {
+            f = (allframes[offset]) * multiplier;
         } else {
             f = 0;
         }
-        format = strtok (NULL, ",");
+        format = strtok(NULL, ",");
         if (format != NULL) {
             formatter = format;
-            if (formatter == "p" ) {
+            if (formatter == "p") {
                 adding = 1;
                 totals = totals + f;
-            } else if (formatter == "t" ) {
-                int time  = (allframes[offset] + (allframes[offset+1]*0x100));
-                int hours = time/60;
-                int mins = time - (hours*60);
-                sprintf(results, "%02d:%02d ",hours,mins);
-            } else if (formatter == "f" ) {
-                Fahrenheit = (f * 1.8 ) + 32;
+            } else if (formatter == "t") {
+                int time = (allframes[offset] + (allframes[offset + 1] * 0x100));
+                int hours = time / 60;
+                int mins = time - (hours * 60);
+                sprintf(results, "%02d:%02d ", hours, mins);
+            } else if (formatter == "f") {
+                Fahrenheit = (f * 1.8) + 32;
                 if (multiplier < 1) {
-                    sprintf(results, "%.1f ",Fahrenheit);
-                } else sprintf(results, "%.0f ",Fahrenheit);
+                    sprintf(results, "%.1f ", Fahrenheit);
+                } else sprintf(results, "%.0f ", Fahrenheit);
             }
         } else if (multiplier < 1) {
-            sprintf(results, "%.1f ",f);
+            sprintf(results, "%.1f ", f);
         } else {
-            sprintf(results, "%.0f ",f);
+            sprintf(results, "%.0f ", f);
         }
 
         if ((formatter != "p") && (adding == 1)) {
             totals = totals + f;
-            sprintf(results, "%.0f ",totals);
+            sprintf(results, "%.0f ", totals);
             adding = 0;
             totals = 0;
         }
@@ -122,45 +120,42 @@ void giveresults(char parray[]) {
     }
 }
 
-unsigned char vbusCalcCRC(const char *buffer, int offset, int length)
-{
+unsigned char vbusCalcCRC(const char * buffer, int offset, int length) {
     unsigned char crc = 0x7F;
     for (int i = 0; i < length; i++) {
-        crc = (crc - buffer [offset + i]) & 0x7F;
+        crc = (crc - buffer[offset + i]) & 0x7F;
     }
     return crc;
 }
 
-int decodeheader()
-{
-    char  buffer[15];
+int decodeheader() {
+    char buffer[15];
     unsigned char a = 0;
     unsigned char b = 0;
-    if (scanf("%c%c%c%c%c%c%c%c%c",&buffer[0],&buffer[1],&buffer[2],&buffer[3],&buffer[4],&buffer[5],&buffer[6],&buffer[7],&buffer[8]) != 1)
-    {
+    if (scanf("%c%c%c%c%c%c%c%c%c", & buffer[0], & buffer[1], & buffer[2], & buffer[3], & buffer[4], & buffer[5], & buffer[6], & buffer[7], & buffer[8]) != 1) {
         a = buffer[8];
-        if ( buffer[4] == 0x10 ) {
-            if ( debug == 1 ) std::cout << "Protocol Version 1.0\n";
+        if (buffer[4] == 0x10) {
+            if (debug == 1) std::cout << "Protocol Version 1.0\n";
             b = vbusCalcCRC(buffer, 0, 8);
-        } else if ( buffer[4] == 0x20 ) {
-            if ( debug == 1 ) std::cout << "Protocol Version 2.0\n";
-            if ( buffer[6] * 0x100  + buffer[5] == 0x0500 ) {
-                if ( debug == 1 ) std::cout << "Broadcast for Clearance\n";
+        } else if (buffer[4] == 0x20) {
+            if (debug == 1) std::cout << "Protocol Version 2.0\n";
+            if (buffer[6] * 0x100 + buffer[5] == 0x0500) {
+                if (debug == 1) std::cout << "Broadcast for Clearance\n";
             }
             b = a;
         } else {
-            if ( debug == 1 ) std::cout << "Unknown Protocol Version\n";
+            if (debug == 1) std::cout << "Unknown Protocol Version\n";
         }
 
-        if (  a == b ) {
-            model = buffer[3] * 0x100  + buffer[2];
-            if ( debug == 1 ) fprintf(stdout,"Model: 0x%02x \n", model);
-            if ( debug == 1 ) fprintf(stdout,"Frames: 0x%01x \n", buffer[7]);
+        if (a == b) {
+            model = buffer[3] * 0x100 + buffer[2];
+            if (debug == 1) fprintf(stdout, "Model: 0x%02x \n", model);
+            if (debug == 1) fprintf(stdout, "Frames: 0x%01x \n", buffer[7]);
             return buffer[7];
         }
     }
 
-    if ( debug == 1 ) std::cout << "Bad Header CRC\n";
+    if (debug == 1) std::cout << "Bad Header CRC\n";
     return 0;
 }
 
@@ -168,65 +163,62 @@ int decodeframe(int x) {
     char buffer[7];
     unsigned char a = 0;
     unsigned char b = 0;
-    if (scanf("%c%c%c%c%c%c",&buffer[0],&buffer[1],&buffer[2],&buffer[3],&buffer[4],&buffer[5]) != 1) {
-        unsigned char t = buffer[0] + buffer[1] + buffer[2] + buffer[3] + buffer[4] ;
-        b = (( ~t | 0x80) - 0x80 );
+    if (scanf("%c%c%c%c%c%c", & buffer[0], & buffer[1], & buffer[2], & buffer[3], & buffer[4], & buffer[5]) != 1) {
+        unsigned char t = buffer[0] + buffer[1] + buffer[2] + buffer[3] + buffer[4];
+        b = ((~t | 0x80) - 0x80);
         a = buffer[5];
-        if ( a == b ) {
-            frame[0] =  buffer[0] + (( buffer[4] & 0x01 ) * 0x80 ) ;
-            frame[1] =  buffer[1] + (( buffer[4] >> 1 & 0x01 ) * 0x80 ) ;
-            frame[2] =  buffer[2] + (( buffer[4] >> 2 & 0x01 ) * 0x80 ) ;
-            frame[3] =  buffer[3] + (( buffer[4] >> 3 & 0x01 ) * 0x80 ) ;
+        if (a == b) {
+            frame[0] = buffer[0] + ((buffer[4] & 0x01) * 0x80);
+            frame[1] = buffer[1] + ((buffer[4] >> 1 & 0x01) * 0x80);
+            frame[2] = buffer[2] + ((buffer[4] >> 2 & 0x01) * 0x80);
+            frame[3] = buffer[3] + ((buffer[4] >> 3 & 0x01) * 0x80);
         } else {
             frame[0] = 0;
             frame[1] = 0;
             frame[2] = 0;
             frame[3] = 0;
-            if ( debug == 1 ) std::cout << "Bad Frame CRC\n";
+            if (debug == 1) std::cout << "Bad Frame CRC\n";
         }
     } else {
-        if ( debug == 1 ) std::cout << "Scanf of buffer failed\n";
+        if (debug == 1) std::cout << "Scanf of buffer failed\n";
     }
     return a - b;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char * argv[]) {
     for (int i = 1; i < argc; i++) {
-        if ((string)argv[i] == "-d") {
+        if ((string) argv[i] == "-d") {
             std::cout << "Enabling debug\n";
             debug = 1;
         }
     }
 
-    char  buffer[2];
+    char buffer[2];
     unsigned char a;
-    char* arg_dup;
-    int framecount,c=0,decodecount=0,decodedcount=0;
+    char * arg_dup;
+    int framecount, c = 0, decodecount = 0, decodedcount = 0;
     FILE * pFile;
-    while (( (decodedcount < decodecount) | (decodecount==0 ) ) && ( fgets(buffer, 2, stdin) != NULL)) {
+    while (((decodedcount < decodecount) | (decodecount == 0)) && (fgets(buffer, 2, stdin) != NULL)) {
         a = buffer[0];
-        if ( a ==  0xAA) {
-            if ( debug == 1 ) std::cout << "SyncByte\n";
+        if (a == 0xAA) {
+            if (debug == 1) std::cout << "SyncByte\n";
             c = 0;
 
             framecount = decodeheader();
-            if (framecount !=0 )
-            {
-                for ( int x = 0; x < framecount ; x++ )
-                {
+            if (framecount != 0) {
+                for (int x = 0; x < framecount; x++) {
                     c = c + decodeframe(x);
-                    allframes[4*x] = frame[0];
-                    allframes[(4*x)+1] = frame[1];
-                    allframes[(4*x)+2] = frame[2];
-                    allframes[(4*x)+3] = frame[3];
+                    allframes[4 * x] = frame[0];
+                    allframes[(4 * x) + 1] = frame[1];
+                    allframes[(4 * x) + 2] = frame[2];
+                    allframes[(4 * x) + 3] = frame[3];
                 }
 
                 // if all crcs are ok
                 decodedcount++;
                 if (c == 0) {
                     string filen = "stdout";
-                    for (int i = 1; i < argc; i++)
-                    {
+                    for (int i = 1; i < argc; i++) {
                         string sw = argv[i];
                         if (sw == "-f") {
                             i++;
@@ -238,7 +230,7 @@ int main(int argc, char* argv[]) {
                             debug = 1;
                         } else if (sw == "-s") {
                             i++;
-                            modelid = strtol(argv[i],NULL,16);
+                            modelid = strtol(argv[i], NULL, 16);
                         } else {
                             if (modelid == 0 || modelid == model) {
 
@@ -246,27 +238,27 @@ int main(int argc, char* argv[]) {
                                 giveresults(arg_dup);
                                 free(arg_dup);
                             } else {
-                                if ( debug == 1 ) std::cout << "Ignoring, model does not match\n";
+                                if (debug == 1) std::cout << "Ignoring, model does not match\n";
                             }
                         }
                     }
                     if (filen == "stdout") {
-                        if ( presults != "" ) {
-                            fprintf(stdout,"%s\n",presults.c_str());
+                        if (presults != "") {
+                            fprintf(stdout, "%s\n", presults.c_str());
                         } else {
                             std::cout << "Result buffer is empty\n";
                         }
                     } else {
-                        pFile = fopen (filen.c_str(),"w");
-                        fprintf (pFile,"%s\n",presults.c_str());
-                        fclose (pFile);
+                        pFile = fopen(filen.c_str(), "w");
+                        fprintf(pFile, "%s\n", presults.c_str());
+                        fclose(pFile);
                     }
                 } else {
-                    if ( debug == 1 ) std::cout << "CRC check of frames failed!\n";
+                    if (debug == 1) std::cout << "CRC check of frames failed!\n";
                 }
                 presults = "";
             } else {
-                if ( debug == 1 ) std::cout << "No Frames\n";
+                if (debug == 1) std::cout << "No Frames\n";
             }
         }
     }
